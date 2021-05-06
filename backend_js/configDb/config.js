6af1badcf6330f375874d775b8f3546addd52087
Carlos israel Rodriguez Ku <israel_rodriguezlol@hotmail.com>
2021-05-06 14:26:52 -0500

    config

---
+++ backend_js/configDb/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    USERNAMEDB: process.env.USERNAMEDB || "carlos_israel",
+    PASSWORDDB: process.env.PASSWORDDB || "carlos_israel123",
+    DATABASE: process.env.DATABASE || "xcaret",
+};
--- backend_js/index.js
+++ backend_js/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const config = require('./configDb/config')
 const app = express();
 app.use(cors());
 
@@ -11,8 +11,7 @@ app.use('/api/', require('./routes/cars'));
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-const CONNECTION_URL = 'mongodb+srv://carlos_israel:carlos_israel123@cluster0.xdxr5.mongodb.net/xcaret?retryWrites=true&w=majority';
+const CONNECTION_URL = `mongodb+srv://${config.USERNAMEDB}:${config.PASSWORDDB}@cluster0.xdxr5.mongodb.net/${config.DATABASE}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Connect ${PORT}`)))
